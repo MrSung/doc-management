5098b0e1280ae191dc6ec000b6c0f54af22f550e
Sungjoon Park <sungjoon512@gmail.com>
2022-05-22 13:15:16 +0900

    move or create other necessary files

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+
+
+const IndexPage: React.FC = () => {
+  return (
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const res = await fetch('http://localhost:3000/api/feed')
+  // const feed = await res.json()
+  const feed = {
+    title: 'Next.js Doc Management',
+  }
+
+  return {
+    props: { feed },
+  }
+}
+
+export default IndexPage
