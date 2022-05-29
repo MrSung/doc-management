1eef4abac4d1cbfd5ae681ed2cb16ee84a9f816d
Sungjoon Park <sungjoon512@gmail.com>
2022-05-29 20:13:29 +0900

    use shorter filename

--- src/pages/api/document/directory.ts
+++ src/pages/api/document/directory.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Directory } from '@prisma/client'
 
 import prisma from '@/libs/prisma'
-import { AllowedMethod, handleException } from '@/utils/api-handler-helper'
+import { AllowedMethod, handleException } from '@/utils/api-helper'
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   let result: Directory | undefined
